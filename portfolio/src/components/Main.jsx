df0cb23f4632bd516851cdd3d244649c0d9cfa15
David Le <davidbooab@hotmail.com>
2024-10-22 22:28:08 +0200

    Added section

--- portfolio/src/components/Main.jsx
+++ portfolio/src/components/Main.jsx
@@ -3,8 +3,10 @@ import image from "../assets/home_image1.png";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faEnvelope } from '@fortawesome/free-solid-svg-icons';
 import { faLinkedin, faGithub } from '@fortawesome/free-brands-svg-icons';
+import { DiHtml5, DiJava, DiCss3, DiJsBadge , DiMysql, DiDatabase, DiFirebase, DiWindows, DiUbuntu, DiApple } from "react-icons/di";
 import foodImage from "../assets/food.png";
-
+import frontEndImage from "../assets/frontend.png";
+import PageIcons from "./Page-icons";
 const Main = () => {
     return (
       <div>
@@ -12,8 +14,8 @@ const Main = () => {
             <div class="flex flex-col justify-center items-center">
             <img src={image} alt="David" class="rounded-full h-60 w-60 object-cover mb-6" />
                 <h1 class="text-6xl font-bold ">Hello, I'm David.</h1>
-                <p class="text-2xl p-2 max-w-xl text-center mb-2">Welcome to my portfolio! I specialize in building secure, efficient tech solutions that bridge business and technology. With expertise in programming, information security, and system design, I help businesses protect their data and optimize operations. 
-                    Explore my projects to see how I turn technical knowledge into impactful solutions in today’s digital world.</p>
+                <p class="text-2xl p-2 max-w-xl text-center mb-2">Welcome to my portfolio! I specialize in building secure, efficient tech solutions that bridge business and technology. With experience in programming, information security, and system design, I help businesses protect their data and optimize operations. 
+                   </p>
                     <p class="text-base">FINAL YEAR BSC. INFORMATIONS SYSTEMS.</p>
                     <p>LINNAEUS UNIVERSITY, SWEDEN.</p>
                     {/* Social Media Icons Section */}
@@ -42,19 +44,25 @@ const Main = () => {
               <p className="text-2xl p-2 max-w-xl leading-relaxed">
                 I'm someone who enjoys the little things in life—like cooking up a new recipe in the kitchen or spending quality time with my friends.
                 Whether it's experimenting with different flavors or hosting casual get-togethers, I love creating experiences that bring people together.
-                When I’m not in the kitchen or with friends, you’ll probably find me gaming.<br></br> I’m always up for trying new things and making the most of everyday moments.
+                When I’m not in the kitchen or with friends, you’ll probably find me spending time with my girl.<br></br> I’m always up for trying new things and making the most of everyday moments.
                 Feel free to reach out or say hello—I'm always open to meeting new people and sharing stories!
               </p>
               <img src={foodImage} alt="food" className="h-96 w-96 object-cover ml-6 rounded-xl"/>
             </div>
            </div> 
         </section>
-        <section className="skills" title="skills-section">
-          
-        
+        <div className="flex flex-col justify-center items-center mt-24">
+          <section className="skills" title="skills-section">
+            <h2 className="text-4xl font-bold">Skills</h2>
+            <div className="flex flex-row items-center mt-20">
+            <PageIcons/>
+            
+              
+            </div>
           </section>
+         </div>
         </div>
-      </div>
+       </div> 
     );
 } 
 
